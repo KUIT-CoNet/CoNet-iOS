80246e144746d0f17420c2cf7be0c4401a1b05d3
anjiniii <117328806+anjiniii@users.noreply.github.com>
2024-01-09 02:19:06 +0900

    Refactor(my-page): 코드 구조 변경

--- CoNet/Main/MyPage/UserInfo/ChangeNameViewController.swift
+++ CoNet/Main/MyPage/UserInfo/ChangeNameViewController.swift
@@ -10,7 +10,6 @@ import Then
 import UIKit
 
 class ChangeNameViewController: UIViewController {
-
     // label: 이름
     let nameLabel = UILabel().then {
         $0.text = "이름"
@@ -74,115 +73,28 @@ class ChangeNameViewController: UIViewController {
         // background color를 white로 설정 (default: black)
         view.backgroundColor = .white
         
-        // layout
-        setUI()
-        
-        // 클릭 이벤트
-        clickEvents()
-        
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        self.navigationController?.navigationBar.isHidden = true
+        addView()
+        layoutConstraints()
+        buttonActions()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.navigationController?.navigationBar.isHidden = false
     }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.navigationController?.navigationBar.isHidden = true
+    }
 
-    func clickEvents() {
+    private func buttonActions() {
         // .editingChanged: editing이 될 때마다 didChangeNameTextField 함수가 호출됩니다.
         self.nameTextField.addTarget(self, action: #selector(self.didChangeNameTextField(_:)), for: .editingChanged)
         // 텍스트필드 클리어버튼
         self.clearButton.addTarget(self, action: #selector(didClickClearButton), for: .touchUpInside)
         // 확인 버튼
         self.nextBtn.addTarget(self, action: #selector(showTabView(_:)), for: .touchUpInside)
-    }
-
-    func setUI() {
-        
-        let safeArea = view.safeAreaLayoutGuide
-        
-        // label: 이름
-        view.addSubview(nameLabel)
-        nameLabel.snp.makeConstraints { make in
-            make.height.equalTo(18)
-            make.top.equalTo(safeArea.snp.top).offset(46)
-            make.leading.equalTo(safeArea.snp.leading).offset(24)
-        }
-        
-        // 이름 텍스트필드
-        view.addSubview(nameTextField)
-        nameTextField.snp.makeConstraints { make in
-            make.height.equalTo(20)
-            make.leading.equalTo(safeArea.snp.leading).offset(24)
-            make.trailing.equalTo(safeArea.snp.trailing).offset(-24)
-            make.top.equalTo(nameLabel.snp.bottom).offset(8)
-        }
-        
-        // textfield underline
-        view.addSubview(underlineView)
-        underlineView.snp.makeConstraints { make in
-            make.height.equalTo(1)
-            make.leading.equalTo(safeArea.snp.leading).offset(24)
-            make.trailing.equalTo(safeArea.snp.trailing).offset(-24)
-            make.top.equalTo(nameTextField.snp.bottom).offset(10)
-        }
-        
-        // textfield clear button
-        view.addSubview(clearButton)
-        clearButton.snp.makeConstraints { make in
-            make.width.equalTo(16)
-            make.height.equalTo(16)
-            make.trailing.equalTo(nameTextField.snp.trailing).offset(0)
-            make.top.equalTo(nameTextField.snp.top).offset(2)
-            make.bottom.equalTo(nameTextField.snp.bottom).offset(-2)
-        }
-        
-        // Component: 느낌표 마크 1
-        view.addSubview(eMarkView1)
-        eMarkView1.snp.makeConstraints { make in
-            make.height.equalTo(12)
-            make.width.equalTo(12)
-            make.leading.equalTo(safeArea.snp.leading).offset(24)
-            make.top.equalTo(underlineView.snp.bottom).offset(12)
-        }
-        
-        // Component: 이름 입력 조건 label 1
-        view.addSubview(nameCondition1)
-        nameCondition1.snp.makeConstraints { make in
-            make.height.equalTo(16)
-            make.leading.equalTo(eMarkView1.snp.trailing).offset(5)
-            make.top.equalTo(underlineView.snp.bottom).offset(10)
-        }
-        
-        // Component: 느낌표 마크 2
-        view.addSubview(eMarkView2)
-        eMarkView2.snp.makeConstraints { make in
-            make.height.equalTo(12)
-            make.width.equalTo(12)
-            make.leading.equalTo(safeArea.snp.leading).offset(24)
-            make.top.equalTo(eMarkView1.snp.bottom).offset(8)
-        }
-        
-        // Component: 이름 입력 조건 label 2
-        view.addSubview(nameCondition2)
-        nameCondition2.snp.makeConstraints { make in
-            make.height.equalTo(16)
-            make.leading.equalTo(eMarkView2.snp.trailing).offset(5)
-            make.top.equalTo(nameCondition1.snp.bottom).offset(4)
-        }
-        
-        // Component: 확인 버튼
-        view.addSubview(nextBtn)
-        nextBtn.snp.makeConstraints { make in
-            make.height.equalTo(52)
-            make.leading.equalTo(safeArea.snp.leading).offset(24)
-            make.trailing.equalTo(safeArea.snp.trailing).offset(-24)
-            make.top.equalTo(nameCondition2.snp.bottom).offset(498)
-        }
     }
     
     // 이름 입력 텍스트필드에 값이 입력될 경우 입력한 값이 이름 조건에 맞는지 확인합니다.
@@ -245,6 +157,94 @@ class ChangeNameViewController: UIViewController {
                     self.navigationController?.popViewController(animated: true)
                 }
             }
+        }
+    }
+}
+
+// addView와 layoutConstraints에 대한 extension
+extension ChangeNameViewController {
+    private func addView() {
+        view.addSubview(nameLabel)
+        view.addSubview(nameTextField)
+        view.addSubview(underlineView)
+        view.addSubview(clearButton)
+        
+        view.addSubview(eMarkView1)
+        view.addSubview(nameCondition1)
+        view.addSubview(eMarkView2)
+        view.addSubview(nameCondition2)
+        
+        view.addSubview(nextBtn)
+    }
+    
+    private func layoutConstraints() {
+        let safeArea = view.safeAreaLayoutGuide
+        
+        // label: 이름
+        nameLabel.snp.makeConstraints { make in
+            make.height.equalTo(18)
+            make.top.equalTo(safeArea.snp.top).offset(46)
+            make.horizontalEdges.equalTo(view.snp.horizontalEdges).offset(24)
+        }
+        
+        // 이름 텍스트필드
+        nameTextField.snp.makeConstraints { make in
+            make.height.equalTo(20)
+            make.leading.equalTo(safeArea.snp.leading).offset(24)
+            make.trailing.equalTo(safeArea.snp.trailing).offset(-24)
+            make.top.equalTo(nameLabel.snp.bottom).offset(8)
+        }
+        
+        // textfield underline
+        underlineView.snp.makeConstraints { make in
+            make.height.equalTo(1)
+            make.leading.equalTo(safeArea.snp.leading).offset(24)
+            make.trailing.equalTo(safeArea.snp.trailing).offset(-24)
+            make.top.equalTo(nameTextField.snp.bottom).offset(10)
+        }
+        
+        // textfield clear button
+        clearButton.snp.makeConstraints { make in
+            make.width.height.equalTo(16)
+            make.trailing.equalTo(nameTextField.snp.trailing).offset(0)
+            make.top.equalTo(nameTextField.snp.top).offset(2)
+            make.bottom.equalTo(nameTextField.snp.bottom).offset(-2)
+        }
+        
+        // Component: 느낌표 마크 1
+        eMarkView1.snp.makeConstraints { make in
+            make.height.width.equalTo(12)
+            make.leading.equalTo(safeArea.snp.leading).offset(24)
+            make.top.equalTo(underlineView.snp.bottom).offset(12)
+        }
+        
+        // Component: 이름 입력 조건 label 1
+        nameCondition1.snp.makeConstraints { make in
+            make.height.equalTo(16)
+            make.leading.equalTo(eMarkView1.snp.trailing).offset(5)
+            make.top.equalTo(underlineView.snp.bottom).offset(10)
+        }
+        
+        // Component: 느낌표 마크 2
+        eMarkView2.snp.makeConstraints { make in
+            make.height.width.equalTo(12)
+            make.leading.equalTo(safeArea.snp.leading).offset(24)
+            make.top.equalTo(eMarkView1.snp.bottom).offset(8)
+        }
+        
+        // Component: 이름 입력 조건 label 2
+        nameCondition2.snp.makeConstraints { make in
+            make.height.equalTo(16)
+            make.leading.equalTo(eMarkView2.snp.trailing).offset(5)
+            make.top.equalTo(nameCondition1.snp.bottom).offset(4)
+        }
+        
+        // Component: 확인 버튼
+        nextBtn.snp.makeConstraints { make in
+            make.height.equalTo(52)
+            make.leading.equalTo(safeArea.snp.leading).offset(24)
+            make.trailing.equalTo(safeArea.snp.trailing).offset(-24)
+            make.bottom.equalTo(view.snp.bottom).offset(-46)
         }
     }
 }
