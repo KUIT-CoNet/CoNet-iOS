54d3ea047ff211786721781a0999e71898ad916e
anjiniii <117328806+anjiniii@users.noreply.github.com>
2024-01-21 02:51:36 +0900

    Refactor(my-page): 로그아웃 팝업 VC 코드 통일된 구조 적용

--- CoNet/Main/MyPage/LogoutPopUpViewController.swift
+++ CoNet/Main/MyPage/LogoutPopUpViewController.swift
@@ -15,12 +15,9 @@ class LogoutPopUpViewController: UIViewController {
     }
     
     // 팝업
-    let popUp = PopUpView()
-                    .withNoDescription(title: "로그아웃 하시겠습니까?",
-                                       leftButtonTitle: "취소",
-                                       leftButtonAction: #selector(dismissPopUp),
-                                       rightButtonTitle: "로그아웃",
-                                       rightButtonAction: #selector(showLoginViewController))
+    let popUp = PopUpView().withNoDescription(title: "로그아웃 하시겠습니까?",
+                                   leftButtonTitle: "취소", leftButtonAction: #selector(dismissPopUp),
+                                   rightButtonTitle: "로그아웃", rightButtonAction: #selector(showLoginViewController))
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,8 +25,13 @@ class LogoutPopUpViewController: UIViewController {
         // background color를 clear로 설정 (default: black)
         view.backgroundColor = .clear
         
+        addView()
         layoutConstraints()
         
+        buttonActions()
+    }
+    
+    private func buttonActions() {
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissPopUp))
         background.addGestureRecognizer(tapGesture)
     }
@@ -39,6 +41,7 @@ class LogoutPopUpViewController: UIViewController {
         dismiss(animated: true, completion: nil)
     }
     
+    // 로그아웃 후, 로그인 화면으로 이동
     @objc func showLoginViewController(_ sender: UIView) {
         self.logout()
         
@@ -49,9 +52,18 @@ class LogoutPopUpViewController: UIViewController {
         sceneDelegate?.changeRootVC(LoginViewController(), animated: false)
     }
     
+    // 로그아웃
     private func logout() {
         let keychain = KeychainSwift()
         keychain.clear()
+    }
+}
+
+// addView, layoutConstraints
+extension LogoutPopUpViewController {
+    private func addView() {
+        view.addSubview(background)
+        view.addSubview(popUp)
     }
     
     // 모든 layout Constraints
@@ -62,7 +74,6 @@ class LogoutPopUpViewController: UIViewController {
     
     // 배경 Constraints
     private func backgroundConstraints() {
-        view.addSubview(background)
         background.snp.makeConstraints { make in
             make.edges.equalToSuperview().inset(0)
         }
@@ -70,14 +81,10 @@ class LogoutPopUpViewController: UIViewController {
     
     // 팝업 Constraints
     private func popUpConstraints() {
-        view.addSubview(popUp)
         popUp.snp.makeConstraints { make in
             make.width.equalTo(view.snp.width).offset(-48)
-            make.leading.equalTo(view.snp.leading).offset(24)
-            make.trailing.equalTo(view.snp.trailing).offset(24)
-            
-            make.centerX.equalTo(view.snp.centerX)
-            make.centerY.equalTo(view.snp.centerY)
+            make.horizontalEdges.equalTo(view.snp.horizontalEdges).offset(24)
+            make.center.equalTo(view.snp.center)
         }
     }
 }
