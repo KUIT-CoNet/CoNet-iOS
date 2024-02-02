e33c0a0cbe37e1f890bd6b1d23255dd2fa9e07cc
ahhyun1015 <121007805+ahhyun1015@users.noreply.github.com>
2024-02-02 18:44:12 +0900

    Refactor(fix-plan-info): 코드 구성 변경, 제약조건 변경, 네비게이션바 설정

--- CoNet/Main/Meeting/Plan/FixPlanInfoViewController.swift
+++ CoNet/Main/Meeting/Plan/FixPlanInfoViewController.swift
@@ -14,13 +14,16 @@ class FixPlanInfoViewController: UIViewController {
     var members: [PlanDetailMember] = []
     
     // 배경 - .clear
-    let scrollview = UIScrollView().then { $0.backgroundColor = .clear }
-    let contentsView = UIView().then { $0.backgroundColor = .clear }
+    let scrollview = UIScrollView().then {
+        $0.backgroundColor = .clear
+    }
     
-    let titleLabel = UILabel().then {
-        $0.text = "약속이 확정되었습니다!"
-        $0.font = UIFont.headline3Bold
-        $0.textColor = UIColor.black
+    let contentsView = UIView().then {
+        $0.backgroundColor = .clear
+    }
+    
+    let xButton = UIButton().then {
+        $0.setImage(UIImage(named: "closeBtn"), for: .normal)
     }
     
     // 약속 이름
@@ -69,23 +72,71 @@ class FixPlanInfoViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .white
         
-        navigationController?.navigationBar.isHidden = true
+        navigationController?.navigationBar.isHidden = false
+        navigationItem.title = "약속이 확정되었습니다!"
         
+        addView()
         layoutConstraints()
         setupCollectionView()
-        
-        completeButton.addTarget(self, action: #selector(didClickCompleteButton), for: .touchUpInside)
+        setupNavigationBar()
+        buttonActions()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        navigationController?.navigationBar.isHidden = true
+        navigationController?.navigationBar.isHidden = false
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        navigationController?.navigationBar.isHidden = true
+        navigationController?.navigationBar.isHidden = false
         
+        getPlanDetail()
+    }
+    
+    private func setupCollectionView() {
+        memberCollectionView.delegate = self
+        memberCollectionView.dataSource = self
+        memberCollectionView.register(MemberCollectionViewCell.self, forCellWithReuseIdentifier: MemberCollectionViewCell.cellId)
+    }
+    
+    func setupNavigationBar() {
+        let xButtonItem = UIBarButtonItem(customView: xButton)
+        navigationItem.leftBarButtonItem = xButtonItem
+    }
+    
+    func buttonActions() {
+        xButton.addTarget(self, action: #selector(xButtonTapped), for: .touchUpInside)
+        completeButton.addTarget(self, action: #selector(didClickCompleteButton), for: .touchUpInside)
+    }
+    
+    @objc private func showBottomSheet() {
+        let bottomSheetViewController = PlanEditDelBottomSheetViewController()
+        bottomSheetViewController.delegate = self
+        // 네비게이션 컨트롤러에 임베드
+        let navigationController = UINavigationController(rootViewController: bottomSheetViewController)
+        navigationController.modalPresentationStyle = .overCurrentContext
+        navigationController.modalTransitionStyle = .crossDissolve
+        present(navigationController, animated: true, completion: nil)
+    }
+    
+    @objc func xButtonTapped() {
+        if let navigationController = self.navigationController {
+            for controller in navigationController.viewControllers {
+                if let meetingMainVC = controller as? MeetingMainViewController {
+                    navigationController.popToViewController(meetingMainVC, animated: true)
+                    return
+                }
+            }
+        }
+    }
+    
+    @objc func didClickCompleteButton() {
+        navigationController?.popViewController(animated: false)
+        timeShareVC?.popPage()
+    }
+    
+    func getPlanDetail() {
         PlanAPI().getPlanDetail(planId: planId) { plans in
             self.nameRow.setText(plans.planName)
             self.dateRow.setText(plans.date)
@@ -95,111 +146,6 @@ class FixPlanInfoViewController: UIViewController {
             self.memberCollectionView.reloadData()
             
             self.layoutConstraints()
-        }
-    }
-    
-    private func setupCollectionView() {
-        memberCollectionView.delegate = self
-        memberCollectionView.dataSource = self
-        memberCollectionView.register(MemberCollectionViewCell.self, forCellWithReuseIdentifier: MemberCollectionViewCell.cellId)
-    }
-    
-    @objc private func showBottomSheet() {
-        let bottomSheetViewController = PlanEditDelBottomSheetViewController()
-        bottomSheetViewController.delegate = self
-        bottomSheetViewController.modalPresentationStyle = .overCurrentContext
-        bottomSheetViewController.modalTransitionStyle = .crossDissolve
-        present(bottomSheetViewController, animated: true, completion: nil)
-    }
-    
-    @objc func didClickCompleteButton() {
-        navigationController?.popViewController(animated: false)
-        timeShareVC?.popPage()
-    }
-    
-    // 전체 layout
-    private func layoutConstraints() {
-        viewConstraints()
-        rowConstraints()
-        memberConstraints()
-    }
-    
-    // scrollview, contentsview layout
-    private func viewConstraints() {
-        view.addSubview(scrollview)
-        scrollview.snp.makeConstraints { make in
-            make.edges.equalToSuperview().inset(0)
-        }
-        
-        scrollview.addSubview(contentsView)
-        contentsView.snp.makeConstraints { make in
-            make.edges.equalTo(scrollview.contentLayoutGuide)
-            make.width.equalTo(scrollview.frameLayoutGuide)
-            make.height.equalTo(1000)
-        }
-        
-        contentsView.addSubview(titleLabel)
-        titleLabel.snp.makeConstraints { make in
-            make.top.equalToSuperview().offset(41)
-            make.centerX.equalToSuperview()
-            make.height.equalTo(22)
-        }
-        
-        // 확인 버튼
-        view.addSubview(completeButton)
-        completeButton.snp.makeConstraints { make in
-            make.height.equalTo(52)
-            make.leading.trailing.equalToSuperview().inset(24)
-            make.bottom.equalTo(view.snp.bottom).offset(-35)
-        }
-    }
-    
-    // row layout
-    private func rowConstraints() {
-        contentsView.addSubview(nameRow)
-        nameRow.snp.makeConstraints { make in
-            make.width.equalToSuperview().offset(-48)
-            make.height.equalTo(54)
-            make.top.equalTo(titleLabel.snp.top).offset(46)
-            make.leading.equalTo(contentsView.snp.leading).offset(24)
-        }
-        
-        contentsView.addSubview(dateRow)
-        dateRow.snp.makeConstraints { make in
-            make.width.equalToSuperview().offset(-48)
-            make.height.equalTo(54)
-            make.top.equalTo(nameRow.snp.bottom).offset(26)
-            make.leading.equalTo(contentsView.snp.leading).offset(24)
-        }
-        
-        contentsView.addSubview(timeRow)
-        timeRow.snp.makeConstraints { make in
-            make.width.equalToSuperview().offset(-48)
-            make.height.equalTo(54)
-            make.top.equalTo(dateRow.snp.bottom).offset(26)
-            make.leading.equalTo(contentsView.snp.leading).offset(24)
-        }
-    }
-    
-    // 참여자 layout
-    private func memberConstraints() {
-        contentsView.addSubview(memberLabel)
-        memberLabel.snp.makeConstraints { make in
-            make.height.equalTo(14)
-            make.top.equalTo(timeRow.snp.bottom).offset(26)
-            make.leading.equalTo(contentsView.snp.leading).offset(24)
-        }
-        
-        contentsView.addSubview(memberCollectionView)
-        memberCollectionView.snp.makeConstraints { make in
-            make.width.equalToSuperview().offset(-48)
-            
-            let memberRow = ceil(Double(members.count) / 2.0)
-            let height = (memberRow * 42) + ((memberRow - 1) * 10)
-            make.height.equalTo(height)
-            
-            make.top.equalTo(memberLabel.snp.bottom).offset(14)
-            make.leading.trailing.equalToSuperview().inset(24)
         }
     }
 }
@@ -253,6 +199,93 @@ extension FixPlanInfoViewController: PlanInfoViewControllerDelegate {
             popUpVC.modalPresentationStyle = .overCurrentContext
             popUpVC.modalTransitionStyle = .crossDissolve
             present(popUpVC, animated: true, completion: nil)
+        }
+    }
+}
+
+// addView, layout
+extension FixPlanInfoViewController {
+    func addView() {
+        view.addSubview(scrollview)
+        scrollview.addSubview(contentsView)
+        view.addSubview(completeButton)
+        contentsView.addSubview(nameRow)
+        contentsView.addSubview(dateRow)
+        contentsView.addSubview(timeRow)
+        contentsView.addSubview(memberLabel)
+        contentsView.addSubview(memberCollectionView)
+    }
+    
+    // 전체 layout
+    private func layoutConstraints() {
+        viewConstraints()
+        rowConstraints()
+        memberConstraints()
+    }
+    
+    // scrollview, contentsview layout
+    private func viewConstraints() {
+        scrollview.snp.makeConstraints { make in
+            make.edges.equalToSuperview().inset(0)
+        }
+        
+        contentsView.snp.makeConstraints { make in
+            make.edges.equalTo(scrollview.contentLayoutGuide)
+            make.width.equalTo(scrollview.frameLayoutGuide)
+            make.height.equalTo(1000)
+        }
+        
+        // 확인 버튼
+        completeButton.snp.makeConstraints { make in
+            make.height.equalTo(52)
+            make.leading.trailing.equalToSuperview().inset(24)
+            make.bottom.equalTo(view.snp.bottom).offset(-35)
+        }
+    }
+    
+    // row layout
+    private func rowConstraints() {
+        let safeArea = view.safeAreaLayoutGuide
+        
+        nameRow.snp.makeConstraints { make in
+            make.width.equalToSuperview().offset(-48)
+            make.height.equalTo(54)
+            make.top.equalTo(safeArea.snp.top).offset(46)
+            make.leading.equalTo(contentsView.snp.leading).offset(24)
+        }
+    
+        dateRow.snp.makeConstraints { make in
+            make.width.equalToSuperview().offset(-48)
+            make.height.equalTo(54)
+            make.top.equalTo(nameRow.snp.bottom).offset(26)
+            make.leading.equalTo(contentsView.snp.leading).offset(24)
+        }
+        
+        timeRow.snp.makeConstraints { make in
+            make.width.equalToSuperview().offset(-48)
+            make.height.equalTo(54)
+            make.top.equalTo(dateRow.snp.bottom).offset(26)
+            make.leading.equalTo(contentsView.snp.leading).offset(24)
+        }
+    }
+    
+    // 참여자 layout
+    private func memberConstraints() {
+        memberLabel.snp.makeConstraints { make in
+            make.height.equalTo(14)
+            make.top.equalTo(timeRow.snp.bottom).offset(26)
+            make.leading.equalTo(contentsView.snp.leading).offset(24)
+        }
+        
+        memberCollectionView.snp.makeConstraints { make in
+            make.width.equalToSuperview().offset(-48)
+            
+            let memberRow = ceil(Double(members.count) / 2.0)
+            let height = (memberRow * 42) + ((memberRow - 1) * 10)
+            make.height.equalTo(height)
+            
+            make.top.equalTo(memberLabel.snp.bottom).offset(14)
+            make.horizontalEdges.equalToSuperview().inset(24)
         }
     }
 }
