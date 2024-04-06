fc2ff92a0b0fbdc683889eccfa4f58a0b732f0bd
slr-09 <ge12090@naver.com>
2024-04-07 02:42:34 +0900

    Design: 구성원 조회를 위한 member cell view

--- CoNet/Main/Meeting/MeetingMain/GetMemberBottomSheet.swift
+++ CoNet/Main/Meeting/MeetingMain/GetMemberBottomSheet.swift
@@ -39,3 +39,59 @@ extension GetMemberBottomSheet {
         }
     }
 }
+
+class memberView: UIView {
+    let userProfileImage = UIImageView().then {
+        $0.image = UIImage(named: "defaultProfile")
+    }
+    
+    let userNickname = UILabel().then {
+        $0.font = UIFont.body1Medium
+        $0.textColor = .textHigh
+        $0.lineBreakMode = .byTruncatingTail
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        viewSetting()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        viewSetting()
+    }
+    
+    private func viewSetting() {
+        // border 설정
+        self.layer.cornerRadius = 50
+        self.layer.borderWidth = 1
+        self.layer.borderColor = UIColor.mainSub2?.cgColor
+        
+        addView()
+        layoutConstraints()
+    }
+    
+    private func addView() {
+        addSubview(userProfileImage)
+        addSubview(userNickname)
+    }
+    
+    private func layoutConstraints() {
+        self.snp.makeConstraints { make in
+            make.height.equalTo(42)
+            make.width.equalTo(164)
+        }
+        
+        userProfileImage.snp.makeConstraints { make in
+            make.width.height.equalTo(30)
+            make.leading.equalToSuperview().offset(8)
+            make.centerY.equalTo(self.snp.centerY)
+        }
+        
+        userNickname.snp.makeConstraints { make in
+            make.width.equalTo(70)
+            make.leading.equalTo(userProfileImage.snp.trailing).offset(12)
+            make.centerY.equalTo(self.snp.centerY)
+        }
+    }
+}
